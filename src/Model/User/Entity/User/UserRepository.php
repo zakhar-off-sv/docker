8934ef26c28a29039102f4258bbd112643c4d9f9
Sergey Zakharov <zakhar.off.sv@gmail.com>
2020-02-10 13:36:42 +0300

    SignUp controller - form & validate - twig (base. & mail.) - implements user repository - implements sender (swift)

--- src/Model/User/Entity/User/UserRepository.php
+++ src/Model/User/Entity/User/UserRepository.php
@@ -4,19 +4,72 @@ declare(strict_types=1);
 
 namespace App\Model\User\Entity\User;
 
-interface UserRepository
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserRepository
 {
-    public function findByConfirmToken(string $token): ?User;
+    private $em;
 
-    public function findByResetToken(string $token): ?User;
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repo;
 
-    public function get(Id $id): User;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(User::class);
+    }
 
-    public function getByEmail(Email $email): User;
+    public function findByConfirmToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['confirmToken' => $token]);
+    }
 
-    public function hasByEmail(Email $email): bool;
+    public function findByResetToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['resetToken.token' => $token]);
+    }
 
-    public function hasByNetworkIdentity(string $network, string $identity): bool;
+    public function get(Id $id): User
+    {
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
 
-    public function add(User $user): void;
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
+
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByNetworkIdentity(string $network, string $identity): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->innerJoin('t.networks', 'n')
+                ->andWhere('n.network = :network and n.identity = :identity')
+                ->setParameter(':network', $network)
+                ->setParameter(':identity', $identity)
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
 }
