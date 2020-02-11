5467c5c9a5d22989c1ed5dccd795f74d682e1291
Sergey Zakharov <zakhar.off.sv@gmail.com>
2020-02-11 15:15:33 +0300

    added maker

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
