a2f2d9719807476cd90eee1219721b25bd7988bf
Sergey Zakharov <zakhar.off.sv@gmail.com>
2020-02-11 15:38:10 +0300

    login form

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
