dd3a043d3bf84bf2412db6f8d2f0aadff98d6f1d
Sergey Zakharov <zakhar.off.sv@gmail.com>
2020-02-12 17:04:40 +0300

    composer require encore (https://symfony.com/doc/current/frontend.html)

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
